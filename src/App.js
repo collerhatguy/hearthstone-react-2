0eec45cccefb4cc33c4263bff22d39c1977e2a4d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-16 09:32:24 -0400

    search page and react router done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ExpansionList from "./components/ExpansionList";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import SearchPage from "./components/SearchPage";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import "./style/style.css";
 
 function App() {
   
   return (
     <Router>
+      <nav>
+        <Link to="/">
+          All Expansions
+        </Link>
+        <Link to="/search-card">
+          Search Page
+        </Link>
+      </nav>
       <Switch>
         <Route path="/" exact component={ExpansionList}/>
+        <Route path="/search-card" component={SearchPage}/>
       </Switch>
     </Router>
   );

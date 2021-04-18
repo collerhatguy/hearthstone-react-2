f10ab85d237d87f400897c5e30cb4391574fd110
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-18 09:54:47 -0400

    added styling to links and search list

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 export default function SearchList({cards}) {
     return (
-        <div>
+        <div className="search-list">
             {cards.map((card, index) => {
                 return <Card card={card} cardVisibility={true} key={index} />
             })}

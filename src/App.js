d56f7f28e8a40a6f3ddfd572d6b95734b63fbca2
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-07-19 07:34:11 -0400

    deleted boiler plate

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ExpansionList from "./components/ExpansionList";
 import SearchPage from "./components/SearchPage";
 import ArtistPage from "./components/ArtistPage";
 
-import React, { useState } from "react";
+import React from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 import useFetchAllExpansions from "./hooks/useFetchAllExpansions";
@@ -11,7 +11,6 @@ import "./style/style.css";
 
 function App() {
   const data = useFetchAllExpansions();
-  const [artist, setArtist] = useState("")
 
   return (
     <Router>
@@ -32,7 +31,6 @@ function App() {
         <Route path="/search-card" component={SearchPage}/>
         <Route path="/artist-list/:artist">
           <ArtistPage 
-            artist={artist} 
             data={data} 
           />
         </Route>

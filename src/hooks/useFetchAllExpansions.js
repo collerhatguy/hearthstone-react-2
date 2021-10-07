ecf2b831a140e9ad1a50d620fb64f3ca3c09ea6d
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-10-07 19:42:34 -0400

    added decoy api for faster loading

--- src/hooks/useFetchAllExpansions.js
+++ src/hooks/useFetchAllExpansions.js
@@ -1,16 +1,13 @@
 
 import { useState, useEffect } from "react"
-import callApi from "../services"
+import axios from "axios"
 
 function useFetchAllExpansions() {
     const [data, setData] = useState()
     const getData = async () => {
         try {
-            const { data } = await callApi().get()
-            const array = convertToArray(data)
-            const filteredArray = filterByExpansion(array)
-            const filteredTwiceArray = filterByCard(filteredArray)
-            setData(filteredTwiceArray)
+            const { data } = await axios.get("https://hearthstone-decoy.herokuapp.com/")
+            setData(data)
         } catch (err) {
             console.log(err)   
         }
@@ -20,61 +17,6 @@ function useFetchAllExpansions() {
     }, [])
 
     return data
-}
-
-const convertToArray = data => 
-        Object.keys(data).map(expansionName => ({
-            name: expansionName,
-            cards: data[expansionName],
-        })
-    )
-
-const filterByExpansion = data => {
-    const expansionMinimum = 50
-    const invalidExpansions = [
-        "Hero Skins",
-        "Credits",
-        "Tavern Brawl",
-        "Battlegrounds",
-    ]
-
-    return data.filter(expansion => {
-        if (expansion.cards.length < expansionMinimum) return false
-        if (invalidExpansions.includes(expansion.name)) return false
-        return true
-    })
-}
-
-const filterByCard = data => {
-    const invalidCardTypes = [
-        "Hero Power", 
-        "Hero", 
-        "Enchantment"
-    ]
-    const invalidCardNames = [
-        "FX", 
-        "Cost", 
-        "NOOOOOOOOOOOO", 
-        "AFK", 
-        "Coin's Vengeance", 
-        "Anomaly"
-    ]
-
-    return data.map(({ name, cards }) => ({
-            name,
-            cards: cards.filter((card, index) => {
-                const { name, type, flavor } = card
-                if (invalidCardNames.includes(name)) return false
-                if (invalidCardTypes.includes(type)) return false
-                if (!flavor) return false
-                if (cards[index + 1]) {
-                    const nextCard = cards[index + 1]
-                    if (nextCard.name === name) return false
-                }
-                return true
-            })
-        })
-    )
 }
 
 export default useFetchAllExpansions

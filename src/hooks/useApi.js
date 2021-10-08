5af4e17d498120b555665bc5e4ac37eaec6c4f27
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-10-07 20:03:14 -0400

    added syntactical suger

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,12 +1,12 @@
 import axios from "axios"
 
-const callApi = () => {
-    return axios.create({
+const useApi = 
+    axios.create({
         baseURL: `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards`,
         headers: {
             "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
             "x-rapidapi-key": process.env.REACT_APP_API_KEY,
         }
     })
-} 
-export default callApi
+
+export default useApi
--- src/hooks/useFetchSearch.js
+++ src/hooks/useFetchSearch.js
@@ -1,25 +1,27 @@
 import { useState } from "react"
 import { useDebounce } from "use-debounce-coolhatguy"
-import callApi from "../services"
+import useApi from "./useApi"
 
 function useFetchSearch(search) {
     const [searchResponse, setSearchResponse] = useState([])
-    const getSearch = async (search) => {
+
+    const getSearch = async search => {
         try {
             if (search.length === 0) return
-            const { data } = await callApi().get(`/search/${search}`)
+            const { data } = await useApi.get(`/search/${search}`)
             if (!data) return searchResponse
-            if (data.length > 9) {
+            if (data.length > 9) 
                 return setSearchResponse(data.slice(0, 10))
-            }
             setSearchResponse(data)
         } catch(error) {
             console.log(error)
         }
     }
+
     useDebounce(() => {
         getSearch(search)
     }, 1000, [search])
+    
     return searchResponse
 }
 export default useFetchSearch
